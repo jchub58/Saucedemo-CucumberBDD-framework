a0ec880ef2552d39b217aea5941764ca2e83be75
Jchub58 <jayavelchellapandi@gmail.com>
2025-12-08 22:49:35 +0530

    build fix

--- src/test/java/com/saucedemo/runner/TestRunner.java
+++ src/test/java/com/saucedemo/runner/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"stepdefinitions"},
+        glue = {"com.saucedemo.stepdefinitions"},
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
